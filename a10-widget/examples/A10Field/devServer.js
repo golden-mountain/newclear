34b58528e974f3003948bbdc2ad8141f9486af1b
Steven Fong <fongkuanghui@gmail.com>
2017-02-07 14:29:19 +0800

    correct port to 3030

--- a10-widget/examples/A10Field/devServer.js
+++ a10-widget/examples/A10Field/devServer.js
@@ -20,7 +20,7 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(4000, function (err) {
+app.listen(3030, function (err) {
   if (err) {
     console.log(err);
     return;
